7532112904467a3af7bd92e2eb27b8d551c75616
Husnain Khurshid <husnainkhurshid@Husnains.local>
2025-10-12 16:43:29 +0500

    chore: bootstrap UltimateCopier project

---
+++ Ultimate Copier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ultimate Copier
+//
+//  Created by Husnain Khurshid on 12/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ultimate Copier/Ultimate_CopierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultimate_CopierApp.swift
+//  Ultimate Copier
+//
+//  Created by Husnain Khurshid on 12/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Ultimate_CopierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
